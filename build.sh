f42e92a5930e96610566eb46f81b821787a573db
Ralph Soika <ralph.soika@imixs.com>
2023-03-12 09:12:12 +0100

    prepare 1.0.0

--- build.sh
+++ build.sh
@@ -3,14 +3,20 @@ echo "***************************************"
 echo "* Build - Open BPMN vsix.....         *"
 echo "***************************************"
 
+echo "==> clean workspace..."
+rm -R open-bpmn.vscode/webview/lib
+rm -R open-bpmn.vscode/webview/node_modules
+rm -R open-bpmn.vscode/extension/lib
+rm -R open-bpmn.vscode/extension/node_modules
+rm -R open-bpmn.vscode/extension/pack
+
 echo "==> build webview..."
 yarn install
 
 echo "==> build VSIX extension..."
-
 cd open-bpmn.vscode/extension
 yarn install
 vsce package --yarn
 cd ../..
 
-echo "Build completed!"
+echo "==> build completed!"
