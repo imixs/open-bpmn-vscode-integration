dfc32cba77ed9028c51de6e6732922575144ed13
Ralph Soika <ralph.soika@imixs.com>
2023-09-01 18:07:19 +0200

    update config
    Issue #26

--- open-bpmn.vscode/webview/src/app.ts
+++ open-bpmn.vscode/webview/src/app.ts
@@ -13,7 +13,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-// import { createWorkflowDiagramContainer } from '@eclipse-glsp-examples/workflow-glsp';
 import { GLSPDiagramIdentifier, GLSPStarter } from '@eclipse-glsp/vscode-integration-webview';
 import '@eclipse-glsp/vscode-integration-webview/css/glsp-vscode.css';
 import { createBPMNDiagramContainer } from '@open-bpmn/open-bpmn-glsp';
